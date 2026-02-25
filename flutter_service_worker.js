c109e05ec0a86e8b1f8b9264adbe733bfd576162
Corey Marques <corey.marques@sage.com>
2026-02-25 15:32:19 -0500

    Update Flutter web build (SW version & JS)
    Refresh Flutter web build artifacts. Updates flutter_bootstrap.js serviceWorkerVersion, updates resource hashes in flutter_service_worker.js (including flutter_bootstrap.js and main.dart.js), and applies the rebuilt/minified changes in main.dart.js (various internal symbol/function renames and runtime/browser compatibility tweaks). These are auto-generated build outputs from a Flutter web build; do not edit manually.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,14 +22,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "cbe13ec1a393c416ce5a5fa2bc51e19f",
+"flutter_bootstrap.js": "59a88f5639a43116baf3e00cfdc8bfcf",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "c9421c5607dbb1e53ebd1ad75f2dfe7f",
 "/": "c9421c5607dbb1e53ebd1ad75f2dfe7f",
-"main.dart.js": "c1822441b668f198cd6aacf0b2d8fecb",
+"main.dart.js": "d337f5e5cd2fdcfe4529a7602a708fa2",
 "manifest.json": "34d7a93a24a9edfdcbc76b9763f73a11",
 "version.json": "fbcc87217d2df64d1f1794fef8b61037"};
 // The application shell files that are downloaded before a service worker can
